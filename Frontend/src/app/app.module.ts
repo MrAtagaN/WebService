36ca0486e009f795ca0385e146195cb4ef40a39d
Plekhanov <atagan@rambler.ru>
2019-08-14 23:56:02 +0300

    UI

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { Page1Component } from './pages/page1/page1.component';
 import { Page2Component } from './pages/page2/page2.component';
 import { HomeComponent } from './pages/home/home.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -14,9 +15,10 @@ import { HomeComponent } from './pages/home/home.component';
     Page2Component,
     HomeComponent
   ],
-  imports: [
+  imports: [  //Регистрация модулей
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/app/pages/page1/page1.component.ts
+++ Frontend/src/app/pages/page1/page1.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {PageContextService} from "../../services/page-context.service";
+import {HttpClient} from "@angular/common/http";
+import {tap} from "rxjs/operators";
+
+interface Employee {
+  id?: String,
+  employee_name?: String,
+  employee_salary?: String,
+  employee_age?: String,
+  profile_image?: String
+}
 
 @Component({
   selector: 'app-page1',
@@ -8,11 +18,18 @@ import {PageContextService} from "../../services/page-context.service";
 })
 export class Page1Component implements OnInit {
 
-  constructor( context : PageContextService) {
+  private employees : Employee[];
+  private httpClient :HttpClient;
+  private testObj : String[]= new Array<String>("sad","aa");
+
+  constructor( context :PageContextService, httpClient :HttpClient) {
     context.pageName = "page1";
+    this.httpClient = httpClient;
   }
 
   ngOnInit() {
+    this.httpClient.get<Employee[]>("http://dummy.restapiexample.com/api/v1/employees").pipe(tap( employee => this.employees = employee)).subscribe();
+
   }
 
 }
